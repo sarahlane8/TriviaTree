808c0a1b59484a42ab473f110818ec381772c917
Sarah Lane <sarahlane8@gmail.com>
2021-06-08 17:56:57 -0600

    Add key for each QuestionCard

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -7,6 +7,7 @@ const Questions = ({ questions }) => {
   const questionCards = questions.map((question, index) => {
     return(
       <QuestionCard
+        key={Date.now()+index}
         id={Date.now()+index}
         question={question.question}
         answer={question.answer}
