ad510c2e750d09a2b71c9a96e03856b279d0d54e
Sarah Lane <sarahlane8@gmail.com>
2021-06-08 13:57:15 -0600

    Complete should display the nav bar test

--- src/cypress/integration/homepage_spec.js
+++ src/cypress/integration/homepage_spec.js
@@ -4,6 +4,11 @@ describe('Homepage', () => {
   })
 
   it('Should display the nav bar', () => {
-
+    cy.get('.nav-bar').should('be.visible')
+      .get('img').should('be.visible')
+      .get('.nav-bar > h1').should('contain', 'Trivia Tree')
+      .get('.nav-links > button').should('have.length', 2)
+      .get('.nav-links > button').eq(0).should('contain', 'Home')
+      .get('.nav-links > button').eq(1).should('contain', 'Saved Questions')
   })
 })
