03b2a16cd0732a296b3dc4b4f50ea4c49d4d419f
Sarah Lane <sarahlane8@gmail.com>
2021-06-09 19:15:00 -0600

    Change navBar buttons to nav links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,29 @@
 import './NavBar.css'
 import treeIconSrc from '../../utils/treeIconSrc'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return(
     <nav className='nav-bar'>
       <img src={treeIconSrc} alt='cartoon tree' />
       <h1>Trivia Tree</h1>
-      <div className='nav-links'>
-        <button>Home</button>
-        <button>Saved Questions</button>
-      </div>
+      <ul className='nav-links'>
+        <li>
+          <NavLink
+            exact to='/'
+            activeStyle={activeStyling}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            exact to='/savedQuestions'
+            activeStyle={activeStyling}>
+            Saved Questions
+          </NavLink>
+        </li>
+
+      </ul>
     </nav>
   )
 }
