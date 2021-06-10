652a0488ccb969e4724d713f032064bcc3032cd2
Sarah Lane <sarahlane8@gmail.com>
2021-06-10 13:17:03 -0600

    Add should save my saved questions when I click the saved questions button test

--- cypress/integration/savedQuestionsDisplay_spec.js
+++ cypress/integration/savedQuestionsDisplay_spec.js
@@ -4,5 +4,20 @@ describe('SavedQuestionsDisplay', () => {
     cy.loadQuiz()
   })
 
-  
+  it('Should show my saved questions when I click on the Saved Questions nav link', () => {
+    cy.get('.flip-card > button').eq(0).click()
+      .get('.flip-card > button').eq(0).should('contain', '⭐️ Saved! ⭐️')
+
+      .get('.flip-card > button').eq(1).click()
+      .get('.flip-card > button').eq(1).should('contain', '⭐️ Saved! ⭐️')
+
+      .get('.nav-links > li > a').eq(1).click()
+
+      .url().should('eq', 'http://localhost:3000/savedQuestions')
+      .get('.questions-grid').should('be.visible')
+      .get('.flip-card').should('have.length', 2)
+
+      .get('.flip-card > button').eq(0).click()
+      .get('.flip-card').should('have.length', 1)
+  })
 })
