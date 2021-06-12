d061e0b21a94ecd721fc76bb09d994038af16985
Sarah Lane <sarahlane8@gmail.com>
2021-06-12 11:45:00 -0600

    Add test for questions display if fetch failed

---
+++ cypress/integration/errorHandling_spec.js
@@ -0,0 +1,23 @@
+describe('Error Handling', () => {
+  beforeEach( () => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Should display an error if the user tries to access a url that doesn\'t exist', () => {
+    cy.visit('http://localhost:3000/madeUpURL')
+      .get('.error-msg').should('contain', 'Sorry, something went wrong! Please return to the homepage!')
+  })
+
+  it('Should tell the user if the fetch failed', () => {
+    cy.intercept('https://opentdb.com/api.php?amount=12&category=9&difficulty=easy',
+      {
+      "response_code": 2,
+      "results": []
+      })
+    cy.loadQuiz()
+      .get('.questions-error-msg').should('contain', 'Sorry, we can\'t find your questions!')
+    })
+
+
+
+})
