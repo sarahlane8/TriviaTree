ee25080c07d5091bb22fb2a3b78acefd7eafc852
Sarah Lane <sarahlane8@gmail.com>
2021-06-14 17:05:40 -0600

    Add user message to questions on how to view answers

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -81,6 +81,7 @@ class App extends Component {
               return(
                 <div>
                   {this.state.loading && !this.state.error && <h2 className='loading-msg'>Loading your questions!</h2>}
+                  {!this.state.loading && !this.state.error && this.state.questions.length && <h2 className='user-msg'>Click and hold a card to view the answer!</h2>}
                   {this.state.error && <h2 className='questions-error-msg'>{errorMsg}</h2>}
                   {!this.state.questions.length && !this.state.loading && <h2 className='questions-error-msg'>{errorMsg}</h2>}
                   <Questions
