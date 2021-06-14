ad3bc9553980a4d0b5bcdb541bc855c558a2034a
Sarah Lane <sarahlane8@gmail.com>
2021-06-14 17:21:07 -0600

    Update styling to form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -20,24 +20,22 @@ class Form extends Component {
     const { getQuestions } = this.props
     return (
       <div className='questions-form'>
-        <label>
-        Choose a category:
-          <select
-            className='selections'
-            value={this.state.category}
-            onChange={this.handleChange}>
-            <option value='general knowledge'>General Knowledge</option>
-            <option value='science and nature'>Science and Nature</option>
-            <option value='math'>Math</option>
-            <option value='mythology'>Mythology</option>
-            <option value='geography'>Geography</option>
-            <option value='animals'>Animals</option>
-            <option value='history'>History</option>
-          </select>
-        </label>
-      <Link to='/questions'>
-        <button onClick={ () => getQuestions(this.state.category) }>QUIZ ME!</button>
-      </Link>
+        <label>Choose a category:</label>
+        <select
+          className='selections'
+          value={this.state.category}
+          onChange={this.handleChange}>
+          <option value='general knowledge'>General Knowledge</option>
+          <option value='science and nature'>Science and Nature</option>
+          <option value='math'>Math</option>
+          <option value='mythology'>Mythology</option>
+          <option value='geography'>Geography</option>
+          <option value='animals'>Animals</option>
+          <option value='history'>History</option>
+        </select>
+        <Link to='/questions'>
+          <button onClick={ () => getQuestions(this.state.category) }>QUIZ ME!</button>
+        </Link>
       </div>
     )
   }
