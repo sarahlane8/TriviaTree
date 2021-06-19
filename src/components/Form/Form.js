2887f5715afe940e136fd9cd93a065e58ca60e2c
Sarah Lane <sarahlane8@gmail.com>
2021-06-19 12:22:17 -0600

    Add progress to convertin Forms from class to functional component with hooks

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,32 +1,22 @@
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import { Component } from 'react'
+import { useState, useEffect } from 'react'
 import './Form.css'
 
 
-class Form extends Component {
-  constructor() {
-    super()
-    this.state = {
-      category: 'general knowledge'
-    }
-  }
+const Form = ({getQuestions}) => {
+  const [ category, setCategory ] = useState('general knowledge')
 
-  handleChange = (event) => {
-    this.setState( {category: event.target.value} )
-  }
-
-  render() {
-    const { getQuestions } = this.props
     return (
       <div className='questions-form'>
         <label>Choose a category:</label>
         <select
           className='selections'
-          value={this.state.category}
-          onChange={this.handleChange}>
-          <option value='general knowledge'>General Knowledge</option>
-          <option value='science and nature'>Science and Nature</option>
+          value={category}
+          onChange={setCategory()}
+          >
+          <option value='general knowledge' onChange={setCategory('general knowledge')}>General Knowledge</option>
+          <option value='science and nature' onChange={setCategory('science and nature')}>Science and Nature</option>
           <option value='math'>Math</option>
           <option value='mythology'>Mythology</option>
           <option value='geography'>Geography</option>
@@ -34,11 +24,11 @@ class Form extends Component {
           <option value='history'>History</option>
         </select>
         <Link to='/questions'>
-          <button onClick={ () => getQuestions(this.state.category) }>QUIZ ME!</button>
+          <button onClick={ () => getQuestions(category) }>QUIZ ME!</button>
         </Link>
       </div>
     )
-  }
+  
 }
 
 Form.propTypes = {
