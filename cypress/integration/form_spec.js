935cb8f798cccfaf88f989c8ecfe4f89a15195cb
Sarah Lane <sarahlane8@gmail.com>
2021-06-19 18:42:12 -0600

    Complete form spec testing based on user input choice of category

--- cypress/integration/form_spec.js
+++ cypress/integration/form_spec.js
@@ -3,11 +3,20 @@ describe('Form', () => {
     cy.fetchGeographyQuestions()
   })
 
-  it('Should fetch questions based on ', () => {
+  it('Should fetch questions based on user input choice for a category', () => {
     cy.visit('http://localhost:3000')
     cy.get('.questions-form > label').should('contain', 'Choose a category:')
       .get('select').select('geography').invoke('val').should('eq', 'geography')
       .get('button').click()
-      // .get('.flip-card').should('have.length', 12)
+      .get('.questions-grid').should('be.visible')
+      .get('.flip-card').should('have.length', 2)
+      .get('.flip-card > button').eq(0).should('contain', 'Save Question')
+      .get('.question-card > p').eq(0).should('contain',
+        'The Alps are a mountain range on which continent?')
+      .get('.flip-card > button').eq(1).should('contain', 'Save Question')
+      .get('.question-card > p').eq(1).should('contain',
+      'Rhode Island is actually located on the US mainland, despite its name')
   })
 })
+
+
